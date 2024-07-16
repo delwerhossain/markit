c9610030e4384892c5759c535c3689d98c02e3fd
delwerhossain <delwerhossain006@gmail.com>
2024-07-16 09:18:26 +0100

    cpanel + export sitting 1.0.0

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 module.exports = {
   reactStrictMode: true,
+  output: "standalone",
+  images: {
+    loader: "imgix",
+    path: "",
+  },
   // eslint: {
   //   ignoreDuringBuilds: true,
   // },
   env: {
     BASE_URL: process.env.BASE_URL,
-  }
-}
+  },
+};
